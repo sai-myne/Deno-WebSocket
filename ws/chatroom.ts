434fb209b1cd48126fff6409caef08a20991942c
sai-myne <ahlusir@gmail.com>
2020-08-13 15:12:39 +0800

    WebSocket Setup

---
+++ ws/chatroom.ts
@@ -0,0 +1,30 @@
+import { WebSocket, isWebSocketCloseEvent } from 'https://deno.land/std/ws/mod.ts';
+import { v4 } from 'https://deno.land/std/uuid/mod.ts';
+
+let sockets = new Map<string, WebSocket>(); 
+
+const chatConnection = async (ws: WebSocket) => {
+  console.log('new socket connection');
+
+  // add new ws connection to map
+  const uid = v4.generate();
+  sockets.set(uid, ws);
+
+    for await(const ev of ws){
+        console.log(ev);
+
+        // delete socket is connection closed
+        if(isWebSocketCloseEvent(ev)){
+            sockets.delete(uid);
+        }
+
+        // create ev object if ev is sting
+        if(typeof ev === 'string'){
+            let evObj = JSON.parse(ev);
+            console.log(evObj);
+        }
+    }
+
+};
+
+export { chatConnection };
